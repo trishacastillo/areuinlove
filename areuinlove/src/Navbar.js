d5f83818221ece6f9de38f0b33b753ea9de039a9
trishacastillo <79441676+trishacastillo@users.noreply.github.com>
2022-07-30 10:16:57 +0800

    ...

---
+++ areuinlove/src/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+      <h1>Castle Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#ff0042',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+    );
+}
+ 
+export default Navbar;
